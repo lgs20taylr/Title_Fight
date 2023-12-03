aae926dcfcaf66b1a7d2679f5af7377dce293c17
lgs20taylr <152493212+lgs20taylr@users.noreply.github.com>
2023-12-03 11:11:44 +0000

    Merged branches

--- main.py
+++ main.py
@@ -4,62 +4,77 @@
 import random
 import time
 
-initepl = {
-  "Arsenal":83,
-  "Manchester City":85,
-  "Liverpool":83,
-  "Aston Villa":80,
-  "Tottenham Hotspur":81,
-  "Manchester United":82,
-  "Newcastle United":80,
-  "Brighton & Hove Albion":77,
-  "West Ham United":78,
-  "Chelsea":80,
-}
-initech = {
-  "Leicster City":75,
-  "Ipswitch Town":69,
-  "West Bromwich Albion":71,
-  "Leeds United":73,
-  "Southampton":73,
-  "Hull City":70,
-  "Preston North End":70,
-  "Cardiff City":70,
-  "Middlesborough":71,
-  "Sunderland":69,
-}
-basedict = {
-  "points":0,
-  "played":0,
-  "won":0,
-  "drawn":0,
-  "lost":0,
-  "gf":0,
-  "ga":0,
-  "gd":0,
-  "rating":0
-}
-def initleague(league):
-  table = {}
-  for i in league:
-    table[i] = basedict
-    table[i]["rating"] = league[i]
-  return table
-def pickleague(leaguechoice):
-  global epl, ech
-  if leaguechoice == "y":
-    epl = initleague(initepl)
-    ech = initleague(initech)
-  else:
-    epl = initleague(initepl)
-pickleague("y")
-print(epl,ech)
+# initepl = {
+#   "Arsenal":83,
+#   "Manchester City":85,
+#   "Liverpool":83,
+#   "Aston Villa":80,
+#   "Tottenham Hotspur":81,
+#   "Manchester United":82,
+#   "Newcastle United":80,
+#   "Brighton & Hove Albion":77,
+#   "West Ham United":78,
+#   "Chelsea":80,
+# }
+# initech = {
+#   "Leicster City":75,
+#   "Ipswitch Town":69,
+#   "West Bromwich Albion":71,
+#   "Leeds United":73,
+#   "Southampton":73,
+#   "Hull City":70,
+#   "Preston North End":70,
+#   "Cardiff City":70,
+#   "Middlesborough":71,
+#   "Sunderland":69,
+# }
+# # basedict = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":0}
 
-def match(team1,team2,league):
+epl = {
+"Arsenal":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
+"Chelsea":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
+"Manchester City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":85},
+"Liverpool":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
+"Aston Villa":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
+"Tottenham Hotspur":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":81},
+"Manchester United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":82},
+"Brighton & Hove Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":77},
+"West Ham United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":78},
+"Newcastle United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
+}
+ech = {
+"Leicster City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":75},
+"Ipswitch Town":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
+"West Bromwitch Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
+"Leeds United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
+"Southhampton":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
+"Hull City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
+"Preston North End":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
+"Cardiff City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
+"Middlesborough":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
+"Sunderland":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
+}
+
+# def initleague(league):
+#   table = {}
+#   for i in league:
+#     table[i] = basedict
+#     table[i]["rating"] = league[i]
+#   return table
+# def pickleague(leaguechoice):
+#   global epl, ech
+#   if leaguechoice == "y":
+#     epl = initleague(initepl)
+#     ech = initleague(initech)
+#   else:
+#     epl = initleague(initepl)
+def simmatch(team1,team2,league):
+  league[team1]["played"] += 1
+  league[team2]["played"] += 1
   score1 = {}
   score2 = {}
   for i in range(1,10):
-    opquality = random.randint(0,100)
+    opquality = random.randint(0,200)
     chance1 = random.randint(0,league[team1]["rating"])
     chance2 = random.randint(0,league[team1]["rating"])
     minutes = random.randint((i*10)-10, i*10)
@@ -69,14 +84,45 @@ def match(team1,team2,league):
       score2[minutes] = 1
   if len(score1) > len(score2):
     result1 = "W"
+    league[team1]["points"] += 3
     result2 = "L"
   elif len(score2) > len(score1):
-    result2 = "W"
     result1 = "L"
+    result2 = "W"
+    league[team2]["points"] += 3
   else:
-    result1, result2 = "D", "D"
-  print(f"{result1} {team1} {len(score1)} - {len(score2)} {team2} {result2}\n{score1} - {score2}")
+    result1 = "D"
+    league[team1]["points"] += 1
+    result2 = "D"
+    league[team2]["points"] += 1
+  print(f"{result1} {team1} {len(score1)} - {len(score2)} {team2} {result2}")
+  return league
+def printtable(table):
+  for i in table:
+    print(i,table[i]["points"],table[i]["played"])
+def generatefixtures(league,length):
+  fixtures = {}
+  bowl = list(league.keys())
+  for weeks in league:
+    while bowl:
+      hometeam = random.choice(bowl)
+      bowl.remove(hometeam)
+      awayteam = random.choice(bowl)
+      bowl.remove(awayteam)
+      fixtures[hometeam] = awayteam
+  return fixtures
 def main():
-  pickleague("n")
-  match("Chelsea","Arsenal",epl)
-main()
+  global epl
+  global ech
+  eplfixtures = generatefixtures(epl,1)
+  echfixtures = generatefixtures(ech,1)
+  for match in eplfixtures:
+    simmatch(match,eplfixtures[match],epl)
+  with open("epl.txt", mode = "w") as file:
+    file.write(str(epl))
+  for match in echfixtures:
+    simmatch(match,echfixtures[match],ech)
+  with open("ecl.txt", mode = "w") as file:
+    file.write(str(ech))
+if __name__ == "__main__":
+  main()
