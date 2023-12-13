016dcc9cf58b8ed368ae4e3c26a132df174cd880
lgs20taylr <152493212+lgs20taylr@users.noreply.github.com>
2023-12-13 13:20:38 +0000

    promotion and relegation worksssssss

--- main.py
+++ main.py
@@ -30,31 +30,41 @@ import time
 # }
 # # basedict = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":0}
 
-epl = {
-"Arsenal":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
-"Chelsea":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
-"Manchester City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":85},
-"Liverpool":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
-"Aston Villa":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
-"Tottenham Hotspur":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":81},
-"Manchester United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":82},
-"Brighton & Hove Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":77},
-"West Ham United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":78},
-"Newcastle United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
-}
-ech = {
-"Leicster City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":75},
-"Ipswitch Town":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
-"West Bromwitch Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
-"Leeds United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
-"Southhampton":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
-"Hull City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
-"Preston North End":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
-"Cardiff City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
-"Middlesborough":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
-"Sunderland":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
+leagues = {
+  "england":{"epl":{
+            "Arsenal":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
+            "Chelsea":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
+            "Manchester City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":85},
+            "Liverpool":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
+            "Aston Villa":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
+            "Tottenham Hotspur":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":81},
+            "Manchester United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":82},
+            "Brighton & Hove Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":77},
+            "West Ham United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":78},
+            "Newcastle United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
+            "Luton Town":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":72},
+            "Burnley":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":74},
+            },
+  "ech":{
+  "Leicster City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":75},
+  "Ipswitch Town":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
+  "West Bromwitch Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
+  "Leeds United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
+  "Southhampton":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
+  "Hull City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
+  "Preston North End":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
+  "Cardiff City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
+  "Middlesborough":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
+  "Sunderland":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
+  },},
 }
 
+
+timings = {
+  "matchintervals":1,
+  "attmatchend":3,
+  "normatchend":0.05,
+}
 
 # def initleague(league):
 #   table = {}
@@ -75,9 +85,11 @@ def simmatch(team1,team2,league,spectating):
   league[team2]["played"] += 1
   score1 = {}
   score2 = {}
-  if watching == True:
+  if watching:
     print(f"Attending Matchday: {team1} vs {team2}.\n")
   for i in range(1,10):
+    if watching:
+      time.sleep(timings["matchintervals"])
     opquality = random.randint(0,180)
     chance1 = random.randint(0,league[team1]["rating"])
     chance2 = random.randint(0,league[team2]["rating"])
@@ -92,7 +104,7 @@ def simmatch(team1,team2,league,spectating):
       score2[minutes] = 1
       league[team2]["gf"] += 1
       league[team1]["ga"] += 1
-      if watching == True:
+      if watching:
         print(f"[{minutes}] GOAL! {team2} scores!\n {team1} {len(score1)} - {len(score2)} {team2}")
   if len(score1) > len(score2):
     result1 = "W"
@@ -110,6 +122,10 @@ def simmatch(team1,team2,league,spectating):
   league[team1]["gd"] = league[team1]["gf"] - league[team1]["ga"]
   league[team2]["gd"] = league[team2]["gf"] - league[team2]["ga"]
   print(f"\n{result1} {team1} {len(score1)} - {len(score2)} {team2} {result2}\n------------------------------------------")
+  if watching:
+    time.sleep(timings["attmatchend"])
+  else:
+    time.sleep(timings["normatchend"])
   return league
 def printtable(table):
   print("Team Name|Points|Played|Goal Difference|Goals For|Goals Against")
@@ -129,8 +145,8 @@ def generatefixtures(league):
 def weekbyweek(lname,lstr):
   lfixtures = {}
   seasonlength = {
-    "epl":36,
-    "ech":18,
+    "epl":9,
+    "ech":9,
   }
   for week in range(seasonlength[lstr]):
     lfixtures[week] = generatefixtures(lname)
@@ -145,9 +161,39 @@ def resetleague(league):
   for i in league:
     league[i]["points"], league[i]["played"], league[i]["won"], league[i]["lost"], league[i]["drawn"],  league[i]["gd"], league[i]["gf"], league[i]["ga"] = 0,0,0,0,0,0,0,0
   return league
+def findtop(league):
+  # temporary solution
+  templeague = league.copy()
+  templeague["Temp"], templeague["temp"] = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":0}, {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":0}
+  top = ["Temp","temp"]
+  for j in range(2):
+    for i in templeague:
+      if templeague[i]["points"] > templeague[top[j]]["points"]:
+        top[j] = i
+    templeague.pop(top[j])
+  return top
+def findbottom(league):
+  templeague = league.copy()
+  # temporary solution
+  templeague["Temp"], templeague["temp"] = {"points":9999,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":0}, {"points":9999,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":0}
+  top = ["Temp","temp"]
+  for j in range(2):
+    for i in templeague:
+      if templeague[i]["points"] < templeague[top[j]]["points"]:
+        top[j] = i
+    templeague.pop(top[j])
+  return top
+def updown(relegated, promoted, topl, botl):
+  for i in relegated:
+    botl[i] = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":topl[i]["rating"]}
+    topl.pop(i)
+  for i in promoted:
+    topl[i] = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":botl[i]["rating"]}
+    botl.pop(i)
+  return botl, topl
 def main():
-  global epl
-  global ech
+  epl = leagues["england"]["epl"]
+  ech = leagues["england"]["ech"]
   morespec = "y"
   runitback = "y"
   while runitback.lower() == "y":
@@ -158,7 +204,7 @@ def main():
     spectating = []
     while morespec.lower() == "y":
       spectating.append(input("Input a team to spectate."))
-      morespec = input("Add another team?")
+      morespec = input("Add another team? y/n")
     for i in eplfixtures:
       for match in eplfixtures[i]:
         simmatch(match,eplfixtures[i][match],epl,spectating)
@@ -173,6 +219,9 @@ def main():
     ech = updaterating(ech)
     printtable(epl)
     printtable(ech)
+    relegated = findbottom(epl)
+    promoted = findtop(ech)
+    epl, ech = updown(relegated, promoted, epl, ech)
     runitback = input("Simulate another season?")
 if __name__ == "__main__":
   main()
