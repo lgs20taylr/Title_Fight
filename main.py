cd455c09e3218dca39c7e223ea2a795be2e0c4b5
lgs20taylr <152493212+lgs20taylr@users.noreply.github.com>
2024-01-16 10:48:31 +0000

    Working on adding all players

--- main.py
+++ main.py
@@ -31,36 +31,63 @@ def retrieveplayerdata(player):
       return i, database[i]
 def teamplayer(league, team):
   return retrieveplayerdata(random.choice(league[team]["squad"]))
+def chalp(stat):
+  return random.randint(stat, 140)
 player1, p1stats = teamplayer(epl, "Manchester City")
 player2, p2stats = teamplayer(epl, "Chelsea")
+goals1 = 0
+goals2 = 0
 pac = 0
 sho = 1
 pas = 2
 dri = 3
 dfn = 4
 phy = 5
-if p1stats[6] == "D":
-  test1 = random.randint(p1stats[dfn], 180)
-  print("D")
-elif p1stats[6] == "M":
-  test1 = random.randint(p1stats[phy], 180)
-  print("M")
+pos1 = p1stats[6]
+pos2 = p2stats[6]
+t1hasball = True
+if pos1 == "M" or pos2 == "M":
+  test1 = chalp(p1stats[phy])
+  test2 = chalp(p2stats[phy])
+  print("M vs M")
+elif pos1 == "D" and pos2 == "A":
+  test1 = chalp(p1stats[dfn])
+  if random.randint(0,1) == 0:
+    test2 = chalp(p2stats[dri])
+  else:
+    test2 = chalp(p2stats[sho])
+  print("D vs A")
+elif pos1 == "A" and pos2 == "D":
+  test2 = chalp(p2stats[dfn])
+  if random.randint(0,1) == 0:
+    test1 = chalp(p1stats[dri])
+  else:
+    test1 = chalp(p1stats[sho])
+  print("A vs D")
 else:
-  test1 = random.randint(p1stats[dfn], 180)
-  print("A")
-if p2stats[6] == "D":
-  test2 = random.randint(p2stats[dfn], 180)
-  print("D")
-elif p2stats[6] == "M":
-  test2 = random.randint(p2stats[phy], 180)
-  print("M")
-else:
-  test2 = random.randint(p2stats[dfn], 180)
-  print("A")
-print(test1, test2)
-if test1 > test2:
-  print(player1, "beat", player2)
-elif test2 > test1:
-  print(player2, "beat", player1)
-else:
-  print("Equal")
+  test1 = chalp(p1stats[pac])
+  test2 = chalp(p2stats[pac])
+  print("A vs A")
+def simchal(t1hasball, goals1, goals2):
+  print(test1, test2)
+  if test1 > test2:
+    print(player1, "beat", player2)
+    if t1hasball and pos2 == "D":
+      goals1 += 1
+    elif not t1hasball:
+      t1hasball = True
+  elif test2 > test1:
+    print(player2, "beat", player1)
+    if not t1hasball and pos1 == "D":
+      goals2 += 1
+    elif t1hasball:
+      t1hasball = False
+  else:
+    print("Equal")
+  return goals1, goals2
+for i in range(5):
+  print(simchal(t1hasball, goals1, goals2))
+  player1, p1stats = teamplayer(epl, "Manchester City")
+  player2, p2stats = teamplayer(epl, "Chelsea")
+  pos1 = p1stats[6]
+  pos2 = p2stats[6]
