59fb2e1a8562ba39cd65b8d1216e32ba7554282d
lgs20taylr <152493212+lgs20taylr@users.noreply.github.com>
2024-02-21 10:53:07 +0000

    Big Update
    Made a big update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Use Semantic Versioning: https://semver.org/
 # Ensure that new code works on Testing branch before committing to Main
 
+import math
 import random
 import time
 
@@ -30,70 +31,143 @@ import time
 # }
 # # basedict = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":0}
 
-epl = {
-"Arsenal":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
-"Chelsea":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
-"Manchester City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":85},
-"Liverpool":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":83},
-"Aston Villa":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
-"Tottenham Hotspur":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":81},
-"Manchester United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":82},
-"Brighton & Hove Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":77},
-"West Ham United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":78},
-"Newcastle United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":80},
-}
-ech = {
-"Leicster City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":75},
-"Ipswitch Town":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
-"West Bromwitch Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
-"Leeds United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
-"Southhampton":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":73},
-"Hull City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
-"Preston North End":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
-"Cardiff City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":70},
-"Middlesborough":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":71},
-"Sunderland":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"rating":69},
+leagues = {
+  "england":{"epl":{
+            "Arsenal":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Bukayo Saka", "Declan Rice", "William Saliba"]},
+            "Chelsea":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Christopher Nkunku", "Enzo Fernandez", "Reece James",]},
+            "Manchester City":{"points":4,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Erling Haaland", "Kevin De Bruyne", "Ruben Dias",]},
+            "Liverpool":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Mohamed Salah","Alexis Mac Allister","Virgil van Dijk",]},
+            "Tottenham Hotspur":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Heung-Min Son", "Giovani Lo Celso", "Cristian Romero",]},
+            "Manchester United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Rasmus Hojlund", "Bruno Fernandes", "Harry Maguire",]},
+            "Luton Town":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Elijah Adebayo", "Ross Barkley", "Ryan Giles",]},
+            "Aston Villa":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Ollie Watkins", "Boubacar Kamara", "Pau Torres",]},
+            },
+    "ech":{
+    "Leicester City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Jamie Vardy","Wilfred Ndidi","Ricardo Pereira"]},
+    "Ipswitch Town":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["George Hirst","Conor Chaplin","Leif Davis",]},
+    "West Bromwitch Albion":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Josh Maja","John Swift","Darnell Furlong",]},
+    "Leeds United":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Georginio Rutter","Glen Kamara","Joe Rondon"]},
+    "Southhampton":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Che Adams","Carlos Alcaraz","Kyle Walker-Peters",]},
+    "Hull City":{"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":["Liam Delap","Jean Seri","Sean McLoughlin"]},
+    },},
 }
 
-
-# def initleague(league):
-#   table = {}
-#   for i in league:
-#     table[i] = basedict
-#     table[i]["rating"] = league[i]
-#   return table
-# def pickleague(leaguechoice):
-#   global epl, ech
-#   if leaguechoice == "y":
-#     epl = initleague(initepl)
-#     ech = initleague(initech)
-#   else:
-#     epl = initleague(initepl)
+commentary = {
+  "miss":[],
+  "goal":[],
+  "end":[],
+  "start":[],
+  "dispos":[],
+}
+database = {}
+def initcom():
+  global commentary
+  for i in commentary:
+    with open((i+".txt"), mode = "r") as file:
+      commentary[i] = file.readlines()
+def initdatabase():
+  global database
+  with open("players.txt", "r+") as file:
+    plist = file.readlines()
+  for i in plist:
+    player = i.split("|")
+    temp = player[1].split(",")
+    pfacts = []
+    for j in temp:
+      if j.isnumeric():
+        pfacts.append(int(j))
+      else:
+        pfacts.append(j)
+    pfacts.pop(-1)
+    database[player[0]] = pfacts
+timings = {
+  "matchintervals":{"normal":0.75,"goal":1.5},
+  "attmatchend":2,
+  "normatchend":0.025,
+}
+def retrieveplayerdata(player):
+  global database  
+  return database[player]
+def chalp(stat):
+  return random.randint(stat, 140)
+def chalres(t1hasball, test1, test2, player1, player2, pos1, pos2):
+  goal1 = False
+  goal2 = False
+  if test1 > test2:
+    if t1hasball and pos2 == "D" and (pos1 == "A" or pos1 == "M" or (pos1 == "D" and random.randint(0,3) == 0)):
+      goal1 = True
+      t1hasball = False
+    elif not t1hasball and not test1 > test2+random.randint(0,30):
+      t1hasball = True
+  elif test2 > test1:
+    if not t1hasball and pos1 == "D" and (pos2 == "A" or pos2 == "M" or (pos2 == "D" and random.randint(0,3) == 0)):
+      goal2 = True
+      t1hasball = True
+    elif t1hasball and test2 > test1+random.randint(0,30):
+      t1hasball = False
+  return t1hasball, goal1, goal2
+def simchal(p1stats, p2stats, pos1, pos2):
+  if pos1 == "M" or pos2 == "M":
+    test1 = chalp(p1stats[5])
+    test2 = chalp(p2stats[5])
+  elif pos1 == "D" and pos2 == "A":
+    test1 = chalp(p1stats[4])
+    test2 = chalp(p2stats[3]+20) if random.randint(0, 1) == 0 else chalp(p2stats[1])
+  elif pos1 == "A" and pos2 == "D":
+    test1 = chalp(p2stats[4])
+    test2 = chalp(p1stats[3]+20) if random.randint(0, 1) == 0 else chalp(p1stats[1])
+  else:
+    test1 = chalp(p1stats[0])
+    test2 = chalp(p2stats[0])
+  return test1, test2
 def simmatch(team1,team2,league,spectating):
   watching = team1 in spectating or team2 in spectating
   league[team1]["played"] += 1
   league[team2]["played"] += 1
   score1 = {}
   score2 = {}
-  if watching == True:
-    print(f"Attending Matchday: {team1} vs {team2}.\n")
-  for i in range(1,10):
-    opquality = random.randint(0,180)
-    chance1 = random.randint(0,league[team1]["rating"])
-    chance2 = random.randint(0,league[team2]["rating"])
-    minutes = random.randint((i*10)-9, i*10)
-    if chance1 > chance2 and chance1 >= opquality:
+  goal1 = False
+  goal2 = False
+  t1hasball = random.randint(0, 1) == 0
+  if watching:
+    commentpick = random.choice(commentary["start"])
+    print(f"Attending Matchday: {team1} vs {team2}.\n\n{commentpick}")
+  for i in range(1,19):
+    if watching and (goal1 or goal2):
+      time.sleep(timings["matchintervals"]["goal"])
+    elif watching:
+      time.sleep(timings["matchintervals"]["normal"])
+    player1 = random.choice(league[team1]["squad"])
+    player2 = random.choice(league[team2]["squad"])
+    p1stats = retrieveplayerdata(player1)
+    p2stats = retrieveplayerdata(player2)
+    pos1 = p1stats[6]
+    pos2 = p2stats[6]
+    test1, test2 = simchal(p1stats,p2stats,pos1,pos2)
+    tempball = t1hasball
+    t1hasball, goal1, goal2 = chalres(t1hasball, test1, test2, player1, player2, pos1, pos2)
+    minutes = random.randint(math.floor((i*5)-4),math.floor(i*5))
+    if goal1:
       score1[minutes] = 1
       league[team1]["gf"] += 1
       league[team2]["ga"] += 1
-      if watching == True:
-        print(f"[{minutes}] GOAL! {team1} scores!\n {team1} {len(score1)} - {len(score2)} {team2}")
-    elif chance2 > chance1 and chance2 >= opquality:
+      if watching:
+        commentpick = random.choice(commentary["goal"])
+        print(f"[{minutes}] GOAL! {player1} scores for {team1}! {commentpick} {team1} {len(score1)} - {len(score2)} {team2}\n")
+    elif goal2:
       score2[minutes] = 1
       league[team2]["gf"] += 1
       league[team1]["ga"] += 1
-      if watching == True:
-        print(f"[{minutes}] GOAL! {team2} scores!\n {team1} {len(score1)} - {len(score2)} {team2}")
+      if watching:
+        commentpick = random.choice(commentary["goal"])
+        print(f"[{minutes}] GOAL! {player2} scores for {team2}! {commentpick}\n {team1} {len(score1)} - {len(score2)} {team2}")
+    elif tempball != t1hasball and watching:
+      commentpick = random.choice(commentary["dispos"])
+      if t1hasball:
+        print(f"[{minutes}] {player1} {commentpick} {player2}.")
+      else:
+        print(f"[{minutes}] {player2} {commentpick} {player1}.")
+    tempball = t1hasball
   if len(score1) > len(score2):
     result1 = "W"
     league[team1]["points"] += 3
@@ -109,16 +183,23 @@ def simmatch(team1,team2,league,spectating):
     league[team2]["points"] += 1
   league[team1]["gd"] = league[team1]["gf"] - league[team1]["ga"]
   league[team2]["gd"] = league[team2]["gf"] - league[team2]["ga"]
+  commentpick = random.choice(commentary["end"])
+  if watching:
+    print(commentpick)
   print(f"\n{result1} {team1} {len(score1)} - {len(score2)} {team2} {result2}\n------------------------------------------")
+  if watching:
+    time.sleep(timings["attmatchend"])
+  else:
+    time.sleep(timings["normatchend"])
   return league
 def printtable(table):
-  print("Team Name|Points|Played|Goal Difference|Goals For|Goals Against")
+  print("Team Name|Points|Played|Goal Difference|Goals For|Goals Against|Rating")
   for i in table:
-    print(i,table[i]["points"],table[i]["played"],table[i]["gd"],table[i]["gf"],table[i]["ga"],table[i]["rating"])
+    print(i,table[i]["points"],table[i]["played"],table[i]["gd"],table[i]["gf"],table[i]["ga"],calcavgrat(table,i))
 def generatefixtures(league):
   fixtures = {}
   bowl = list(league.keys())
-  for weeks in league:
+  for _weeks in league:
     while bowl:
       hometeam = random.choice(bowl)
       bowl.remove(hometeam)
@@ -129,50 +210,169 @@ def generatefixtures(league):
 def weekbyweek(lname,lstr):
   lfixtures = {}
   seasonlength = {
-    "epl":36,
-    "ech":18,
+    "epl":14,
+    "ech":10,
   }
   for week in range(seasonlength[lstr]):
     lfixtures[week] = generatefixtures(lname)
   return lfixtures
+def playerrat(player):
+  global database
+  cum = 0
+  statcum = 0
+  pstats = database[player].copy()
+  pos = pstats[6]
+  if pos == "A":
+    cum = pstats[0]+pstats[1]+pstats[3]
+    statcum = 3
+  elif pos == "M":
+    cum = pstats[2]+pstats[5]
+    statcum = 2
+  elif pos == "D":
+    cum = pstats[4]+pstats[5]
+    statcum = 2
+  return cum, statcum
+def calcavgrat(league, team):
+  cum = 0
+  statcum = 0
+  for player in league[team]["squad"]:
+    cum, statcum = playerrat(player)
+  return cum // statcum
+def radj():
+  return random.randint(-3,3)
 def updaterating(league):
+  agebonus = {16:8,17:6,18:5,19:5,20:5,21:4,22:3,23:2,24:1,25:1,26:0,27:0,28:0,29:0,30:-1,31:-2,32:-3,33:-4,34:-5,35:-6,36:-7,37:-8,38:-9,39:-10,40:-12}
+  global database
   for team in league:
-    league[team]["rating"] += round((league[team]["gd"]/4) + (league[team]["won"]/2))
-    if league[team]["rating"] < 30:
-      league[team]["rating"] = 30
+    rchan = round((league[team]["gd"]/4) + (league[team]["points"]/2))
+    for player in league[team]["squad"]:
+      position = database[player][6]
+      age = database[player][8]
+      if position == "A":
+        database[player][0] += rchan + radj() + agebonus[age]
+        database[player][1] += rchan + radj() + agebonus[age]
+        database[player][3] += rchan + radj() + agebonus[age]
+      elif position == "M":
+        database[player][2] += rchan + radj() + agebonus[age]
+        database[player][5] += rchan + radj() + agebonus[age]
+      elif position == "D":
+        database[player][4] += rchan + radj() + agebonus[age]
+        database[player][5] += rchan + radj() + agebonus[age]
+      database[player][8] += 1
+      for stat in range(len(database[player])-3):
+        if database[player][stat] < 10:
+          database[player][stat] = 10
+        elif database[player][stat] >= 120:
+          database[player][stat] = 120
   return league
 def resetleague(league):
   for i in league:
     league[i]["points"], league[i]["played"], league[i]["won"], league[i]["lost"], league[i]["drawn"],  league[i]["gd"], league[i]["gf"], league[i]["ga"] = 0,0,0,0,0,0,0,0
   return league
+def findtop(league):
+  # temporary solution
+  templeague = league.copy()
+  templeague["Temp"], templeague["temp"] = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":[]}, {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":[]}
+  top = ["Temp","temp"]
+  for j in range(2):
+    for i in templeague:
+      if templeague[i]["points"] > templeague[top[j]]["points"]:
+        top[j] = i
+    templeague.pop(top[j])
+  return top
+def findbottom(league):
+  templeague = league.copy()
+  # temporary solution
+  templeague["Temp"], templeague["temp"] = {"points":9999,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":[]}, {"points":9999,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":[]}
+  top = ["Temp","temp"]
+  for j in range(2):
+    for i in templeague:
+      if templeague[i]["points"] < templeague[top[j]]["points"]:
+        top[j] = i
+    templeague.pop(top[j])
+  return top
+def updown(relegated, promoted, topl, botl):
+  for i in relegated:
+    botl[i] = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":topl[i]["squad"]}
+    topl.pop(i)
+  for i in promoted:
+    topl[i] = {"points":0,"played":0,"won":0,"drawn":0,"lost":0,"gf":0,"ga":0,"gd":0,"squad":botl[i]["squad"]}
+    botl.pop(i)
+  return botl, topl
+def pickmatchsquad(team):
+  squad = ["Bad Guy","Bad Guy","Bad Guy"]
+  for player in team["squad"]:
+    rat = playerrat(player)
+    pos = database[player][6]
+    if pos == "D" and rat > playerrat(squad[0]):
+      squad[0] = player
+    elif pos == "M" and rat > playerrat(squad[1]):
+      squad[1] = player
+    elif pos == "A" and rat > playerrat(squad[2]):
+      squad[2] = player
+  return squad
+def handletrans(origin, player, dest, epl, ech):
+  oleague = epl if origin in epl else ech if origin in ech else False
+  dleague = epl if dest in epl else ech if dest in ech else False
+  if player in database:
+    if oleague:
+      oleague[origin]["squad"].remove(player)
+    if dleague:
+      dleague[dest]["squad"].append(player)
+      database[player][7] = dest
 def main():
-  global epl
-  global ech
-  morespec = "y"
+  initdatabase()
+  epl = leagues["england"]["epl"]
+  ech = leagues["england"]["ech"]
   runitback = "y"
+  initcom()
   while runitback.lower() == "y":
     epl = resetleague(epl)
     ech = resetleague(ech)
     eplfixtures = weekbyweek(epl,"epl")
     echfixtures = weekbyweek(ech,"ech")
     spectating = []
+    morespec = "y"
     while morespec.lower() == "y":
       spectating.append(input("Input a team to spectate."))
-      morespec = input("Add another team?")
+      morespec = input("Add another team? y/n")
     for i in eplfixtures:
       for match in eplfixtures[i]:
         simmatch(match,eplfixtures[i][match],epl,spectating)
+      print("###############################\nEnglish Premier League")
+      printtable(epl)
       with open("epl.txt", mode = "w") as file:
         file.write(str(epl))
     for i in echfixtures:
       for match in echfixtures[i]:
         simmatch(match,echfixtures[i][match],ech,spectating)
+      print("###############################\nEFL Championship")
+      printtable(ech)
       with open("ech.txt", mode = "w") as file:
         file.write(str(ech))
+    print("###############################\nEnglish Premier League")
+    printtable(epl)
+    print("###############################\nEFL Championship")
+    printtable(ech)
     epl = updaterating(epl)
     ech = updaterating(ech)
-    printtable(epl)
-    printtable(ech)
+    relegated = findbottom(epl)
+    promoted = findtop(ech)
+    ech, epl = updown(relegated, promoted, epl, ech)
+    # printdatabase(database)
+    maketrans = bool(input("Make transfers?").lower() == "y")
+    while maketrans:
+      print("Transfer Window Open")
+      handletrans(input("Input Origin Team or FA if free agent"), input("Input Player"), input("Input Destination Team"), epl, ech)
+      maketrans = input("Make another transfer?").lower() == "y"
+    printdatabase(database)
     runitback = input("Simulate another season?")
+def printdatabase(database):
+  for i in database:
+    print(i, database[i])
+# initdatabase()
+# for i in database:
+#   if len(database[i]) < 7:
+#     print(i)
 if __name__ == "__main__":
   main()
